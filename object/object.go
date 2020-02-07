62aacb6353e7ce73290ad9c8781619bb4a6d8fb1
Kazuki HAra <yukidama.k@gmail.com>
2020-02-07 00:17:49 +0000

    Finish 4.3.0

--- object/object.go
+++ object/object.go
@@ -17,6 +17,7 @@ const (
 	ERROR_OBJ        = "ERROR"
 	FUNCTION_OBJ     = "FUNCTION"
 	STRING_OBJ       = "STRING"
+	BUILTIN_OBJ      = "BUILTIN"
 )
 
 type Object interface {
@@ -27,6 +28,8 @@ type Object interface {
 type Integer struct {
 	Value int64
 }
+
+type BuiltinFunction func(args ...Object) Object
 
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
@@ -75,7 +78,7 @@ type Environment struct {
 
 func (e *Environment) Get(name string) (Object, bool) {
 	obj, ok := e.store[name]
-	if !ok  && e.outer != nil {
+	if !ok && e.outer != nil {
 		obj, ok = e.outer.Get(name)
 	}
 
@@ -118,3 +121,10 @@ type String struct {
 
 func (s *String) Type() ObjectType { return STRING_OBJ }
 func (s *String) Inspect() string  { return s.Value }
+
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func (b *Builtin) Type() ObjectType { return BUILTIN_OBJ }
+func (b *Builtin) Inspect() string  { return "builtin function" }
